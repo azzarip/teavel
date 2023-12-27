d58821fd7086f39ee9260c2a6475470f5365172c
azzarip <azzarip@users.noreply.github.com>
2023-12-27 13:57:59 +0000

    Fix styling

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -5,9 +5,10 @@ namespace Azzarip\Teavel\Database\Factories;
 use Azzarip\Teavel\Models\Contact;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class ContactFactory extends Factory 
+class ContactFactory extends Factory
 {
     protected $model = Contact::class;
+
     public function definition(): array
     {
         return [
