7f06ac3de77c64c016c58089772b6c11efeebc97
Azzarip <paride21azzari@gmail.com>
2024-03-20 11:26:59 +0100

    ContactFromData

--- src/Models/Contact.php
+++ src/Models/Contact.php
@@ -2,12 +2,13 @@
 
 namespace Azzarip\Teavel\Models;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\Access\Authorizable;
-use Illuminate\Support\Str;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
 class Contact extends Model implements AuthenticatableContract, AuthorizableContract
 {
@@ -39,6 +40,29 @@ class Contact extends Model implements AuthenticatableContract, AuthorizableCont
             $model->uuid = Str::uuid();
         });
     }
+
+public static function fromData(array $data)
+{
+    $email = $data['email'];
+    $contact = self::findByEmail($email);
+
+    if(!$contact) {
+        $data['privacy_at'] = now();
+        if(!Arr::exists($data, 'not_marketing')) {
+            $data['marketing_at'] = now();
+        }
+        Arr::forget($data, ['privacy', 'not_marketing']);
+        return self::create($data);
+    }
+
+    if(empty($contact->last_name)){$contact->last_name = $data['last_name'];}
+    if(empty($contact->phone))    {$contact->phone = $data['phone'];}
+    if(empty($contact->marketing_at) && !Arr::exists($data, 'not_marketing')){$contact->marketing_at = now();}
+
+    $contact->save();
+    return $contact;
+}
+
 
 
 }
---
+++ tests/ContactFromDataTest.php
@@ -0,0 +1,85 @@
+<?php
+
+use Illuminate\Support\Arr;
+use Azzarip\Teavel\Models\Contact;
+
+beforeEach(function() {
+    $this->data = [
+        'first_name' => '::name::',
+        'last_name' => '::last_name::',
+        'phone' => '+41787123123',
+        'email' => 'test@email.com',
+    ];
+});
+it('creates a new contact', function () {
+    $contact = Contact::fromData($this->data);
+    $this->assertDatabaseHas('contacts', $this->data);
+});
+
+it('returns a contact', function () {
+    $contact = Contact::fromData($this->data);
+    expect($contact)->toBeInstanceOf(Contact::class);
+});
+
+it('has marketing at = now()', function () {
+    $contact = Contact::fromData($this->data);
+    expect($contact->marketing_at)->not->toBeNull();
+});
+
+test('no new contact for existing email', function () {
+    $data1 = $this->data;
+    $data2 = [
+        'first_name' => '::another_name::',
+        'last_name' => '::another_last_name::',
+        'email' => 'test@email.com',
+    ];
+
+    Contact::fromData($data1);
+    Contact::fromData($data2);
+    $this->assertDatabaseHas('contacts', $data1);
+    $this->assertDatabaseMissing('contacts', $data2);
+});
+
+it('adds last_name if empty', function () {
+    $this->data['last_name'] = null;
+    $contact = Contact::fromData($this->data);
+    expect($contact->last_name)->toBeNull();
+
+    $this->data['last_name'] = '::last_name::';
+    $contact = Contact::fromData($this->data);
+    expect($contact->last_name)->toBe('::last_name::');
+
+});
+
+it('adds phone if empty', function () {
+    $this->data['phone'] = null;
+    $contact = Contact::fromData($this->data);
+    expect($contact->phone)->toBeNull();
+
+    $this->data['phone'] = '+41787878787';
+    $contact = Contact::fromData($this->data);
+    expect($contact->phone)->toBe('+41787878787');
+
+});
+
+
+
+it('DOES NOT add marketing_at if not_marketing', function () {
+    $this->data['not_marketing'] = true;
+    $contact = Contact::fromData($this->data);
+    expect($contact->marketing_at)->toBeNull();
+
+    $contact = Contact::fromData($this->data);
+    expect($contact->marketing_at)->toBeNull();
+});
+
+it('adds marketing_at if empty ', function () {
+    $this->data['not_marketing'] = true;
+    $contact = Contact::fromData($this->data);
+    expect($contact->marketing_at)->toBeNull();
+
+    Arr::forget($this->data, 'not_marketing');
+    $contact = Contact::fromData($this->data);
+    expect($contact->marketing_at)->not->toBeNull();
+
+});
