f07dfa5822b73d56db1cc07177dfbe7159f94a68
Azzarip <paride21azzari@gmail.com>
2024-08-14 16:43:20 +0200

    has get utms

--- src/Automations/EmailAutomation.php
+++ src/Automations/EmailAutomation.php
@@ -8,11 +8,23 @@ class  EmailAutomation extends GoalAutomation
 {
     const TRANSACTIONAL = false;
 
+    protected $utm_campaign;
+    protected $utm_content;
+
     public static function getContentPath() {
         $path = app_path(str_replace('App\\', '/', self::class));
         $file = str_replace(app_path('Teavel\Emails'), base_path('content\emails'), $path) . '.md';
         return $file;
     }
 
+    protected function getUtms()
+    {
+        return [
+            'utm_source' => 'crm',
+            'utm_medium' => 'email',
+            'utm_campaign' => $this->utm_campaign,
+            'utm_content' => $this->utm_content,
+        ];
+    }
 
 }
