13ba3a72aaa1a64b9c797c88219ab7b993e850bf
Azzarip <paride21azzari@gmail.com>
2024-08-18 21:37:43 +0200

    teavel mail update

--- src/Mail/TeavelMail.php
+++ src/Mail/TeavelMail.php
@@ -9,42 +9,19 @@ use Illuminate\Mail\Mailables\Address;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Mail\Mailables\Envelope;
 
-class TeavelMail extends Mailable
+class TeavelMail extends TeavelMailable
+
 {
     use Queueable;
     use SerializesModels;
 
-    public function __construct(public Contact $contact, string $filename, protected array $data = [])
+    public function __construct(string $filename, protected array $data = [])
     {
-        $content = new EmailContent($filename);
-
-        $this->subject = $content->subject;
-
-        $this->html = $this->parseText($content->html);
     }
 
 
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            to: [new Address($this->contact->email, $this->contact->full_name)],
-        );
-    }
-
-
-    protected function parseText($text)
-    {
-        $loader = new \Twig\Loader\ArrayLoader(['email' => $text]);
-        $twig = new \Twig\Environment($loader, ['autoescape' => false]);
-
-        $html = $twig->render('email', [
-            'contact' => $this->contact,
-        ] + $this->data);
-
-        return $html;
+    protected function getFilename() {
+        return $this->filename;
     }
 
 
--- src/Mail/TeavelMailable.php
+++ src/Mail/TeavelMailable.php
@@ -2,20 +2,53 @@
 
 namespace Azzarip\Teavel\Mail;
 
+use Twig\Environment;
+use Twig\Loader\ArrayLoader;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Envelope;
 use Azzarip\Teavel\Models\Contact;
+use Illuminate\Mail\Mailable;
 
-class  TeavelMailable
+class TeavelMailable extends Mailable
 {
     protected $filename;
+
+    protected Contact $contact;
 
     protected array $data;
 
 
-    public function to(Contact $contact) {
-        return new TeavelMail($contact, $this->getFilename(), $this->data);
+    public function toContact(Contact $contact) {
+        $content = new EmailContent($this->getFilename());
+
+        $this->subject = $content->subject;
+
+        $this->html = $this->parseText($content->html);
     }
 
     protected function getFilename() {
         return base_path() . '/' . $this->filename;
     }
+
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            to: [new Address($this->contact->email, $this->contact->full_name)],
+        );
+    }
+
+
+    protected function parseText($text)
+    {
+        $loader = new ArrayLoader(['email' => $text]);
+        $twig = new Environment($loader, ['autoescape' => false]);
+
+        $html = $twig->render('email', [
+            'contact' => $this->contact,
+        ] + $this->data);
+
+        return $html;
+    }
+
 }
