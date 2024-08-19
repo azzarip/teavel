7ce77f197b78e73c282aa4cd1492ae95fd9aa5b7
Azzarip <paride21azzari@gmail.com>
2024-08-19 21:03:10 +0200

    added password

--- resources/views/components/auth/password-success.blade.php
+++ resources/views/components/auth/password-success.blade.php
@@ -3,7 +3,7 @@
         <x-heroicon-o-envelope class="w-20 h-20 text-center text-black" />
     </div>
 
-    <h1 class="text-3xl leading-normal text-center text-black">Password Reset Sent!</h1>
+    <h1 class="mb-1 mb-2 text-3xl leading-normal text-center text-black">Password Reset Sent!</h1>
     <p><span class="font-semibold">Check your inbox</span>, or spam folder. The email contains a link to reset your password.</p>
 
     <p class="mt-4">If the email does not arrive, probably, you are not registered!</p>
