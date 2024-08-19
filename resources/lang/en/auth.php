6107a9c37e5fddd2003825fc0b24cd8494707d59
Azzarip <paride21azzari@gmail.com>
2024-08-19 22:32:54 +0200

    added message succes

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -21,6 +21,9 @@ return [
         'user' => 'Email and Password do not match. Please try again.',
     ],
     'message' => [
-        'request' => 'Type in your email, if you are already registered, you will receive an email to reset your password.'
+        'request' => 'Type in your email, if you are already registered, you will receive an email to reset your password.',
+        'success_1' => '<span class="font-semibold">Check your inbox</span>, or spam folder. The email contains a link to reset your password.',
+        'success_2' => 'If the email does not arrive, probably, you are not registered.',
     ]
 ];
+
--- resources/views/components/auth/password-success.blade.php
+++ resources/views/components/auth/password-success.blade.php
@@ -4,8 +4,8 @@
     </div>
 
     <h1 class="mb-2 text-3xl leading-normal text-center text-black">@lang('teavel::auth.title.reset')</h1>
-    <p><span class="font-semibold">Check your inbox</span>, or spam folder. The email contains a link to reset your password.</p>
+    <p>@lang('teavel::auth.message.success_1')</p>
 
-    <p class="mt-4">If the email does not arrive, probably, you are not registered!</p>
+    <p class="mt-4">@lang('teavel::auth.message.success_2')</p>
     <a href="/register" class="block mt-4 text-center underline">@lang('teavel::auth.link.success_register')</a>
 </div>
