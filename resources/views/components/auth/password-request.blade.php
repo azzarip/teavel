90ff61736004eaf18ee5797c65f39ea60973fb55
Azzarip <paride21azzari@gmail.com>
2024-08-19 20:52:57 +0200

    added two views

---
+++ resources/views/components/auth/password-request.blade.php
@@ -0,0 +1,30 @@
+<div>
+    <h1 class="text-3xl leading-normal text-center text-black">Reset Password</h1>
+
+    <form action="{{ route('password.request') }}" method="POST">
+        @csrf
+        <div class="pt-4 space-y-4">
+            @error('token')
+                <p class="px-2 py-1 text-black bg-red-300 rounded-xl"><x-heroicon-o-exclamation-triangle class="inline w-6 aspect-auto" />
+                The token has expired, please request a new one here.</p>
+            @enderror
+
+            <div>
+                <label for="email" class="">Email:</label>
+                <input type="text" id="email" name="email"
+                    class="w-full p-2 border border-gray-400 rounded-md outline-none hover:border-green-600 focus:border-green-600"
+                    placeholder="Your email" aria-label="Email" autocomplete="email" />
+            </div>
+
+            @error('email')
+            <p class="error-msg">{{ $message }}</p>
+            @enderror
+            <p>Type in your email, if you are already registered, you will receive an email to reset your password.</p>
+            <div class="pt-4">
+                <button type="submit" class="block w-full max-w-lg py-3 mx-auto text-2xl button-34"> Reset Password
+                </button>
+            </div>
+        </div>
+    </form>
+    <a class="block mt-4 text-center underline cursor-pointer" href={{ route('login') }}>Back to Login</a>
+</div>
---
+++ resources/views/components/auth/password-success.blade.php
@@ -0,0 +1,11 @@
+<div class="mt-2 mb-6">
+    <div class="mx-auto w-fit">
+        <x-heroicon-o-envelope class="w-24 h-24 text-center text-black" />
+    </div>
+
+    <h1 class="text-4xl leading-normal text-center text-black">Password Reset Sent!</h1>
+    <p><span class="font-semibold">Check your inbox</span>, or spam folder. The email contains a link to reset your password.</p>
+
+    <p class="mt-4">If the email does not arrive, probably, you are not registered!</p>
+    <a href="/register" class="block mt-4 text-center underline">If you are not registered, click here!</a>
+</div>
