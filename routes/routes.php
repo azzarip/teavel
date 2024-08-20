799473244f4cfea551ce37a186f612de3b26d7fb
Azzarip <paride21azzari@gmail.com>
2024-08-20 16:14:46 +0200

    changed routes

--- routes/routes.php
+++ routes/routes.php
@@ -21,10 +21,10 @@ Route::get('/tvl/{contactUuid}/email/{emailUuid}/{action}', ClickController::cla
 //**   AUTH ROUTES  */
 Route::middleware(['guest'])->group(function () {
     Route::view('/login', config('teavel.auth_views.login'))->name('login');
-    Route::view('/register', config('teavel.auth_views.register'))->name('register');
-    Route::view('/password/request', config('teavel.auth_views.password_request_form'))->name('password.request');
-    Route::view('/password/success', config('teavel.auth_views.password_request_success'))->name('password.success');
-    Route::view('/password/reset', config('teavel.auth_views.password_reset_form'))->name('password.reset');
+
+    Route::view('/password/request', config('teavel.auth_views.password_request'))->name('password.request');
+    Route::view('/password/success', config('teavel.auth_views.password_success'))->name('password.success');
+    Route::view('/password/reset', config('teavel.auth_views.password_reset'))->name('password.reset');
 
     Route::middleware(['throttle:5'])->group(function () {
         Route::post('/password/request', [PasswordController::class, 'request']);
