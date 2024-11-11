b587e3060645a863fc0b6c5f3ad4de20d8c2ff71
Azzarip <paride21azzari@gmail.com>
2024-11-11 22:47:09 +0100

    can suggest value

--- resources/views/forms/field/email.blade.php
+++ resources/views/forms/field/email.blade.php
@@ -1,8 +1,9 @@
+@props(['email' => null])
 <div>
     <label for="email" class="block">Email:</label>
     <input type="text" id="email" name="email"
         class="input-text"
-        placeholder="@lang('teavel::forms.email.placeholder')" autocomplete="email" value="{{ old('email') }}"
+        placeholder="@lang('teavel::forms.email.placeholder')" autocomplete="email" value="{{ $email ?? old('email') }}"
         required />
     @error('email')
         <p class="error-msg">{{ $message }}</p>
