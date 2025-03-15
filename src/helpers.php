b8e40e9ef1bbc7ee306fca6d4a7fd0cbcc8d499e
Azzarip <paride21azzari@gmail.com>
2025-03-15 17:59:48 +0100

    removed unused helper

--- src/helpers.php
+++ src/helpers.php
@@ -1,12 +1,1 @@
 <?php
-
-if (! function_exists('ns_case')) {
-    /**
-     * Convert a namespace-like string to a "namespace case" string
-     * by replacing hyphens and underscores with capital Letters.
-     */
-    function ns_case(string $input): string
-    {
-        return str_replace(' ', '', ucwords(str_replace(['-', '_'], ' ', $input)));
-    }
-}
