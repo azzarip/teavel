b962f40c935db8647b80d7ae6bc642d01b057e1c
Azzarip <paride21azzari@gmail.com>
2025-04-14 20:29:34 +0200

    has shop auth mw

---
+++ src/Http/Middleware/ShopAuth.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Azzarip\Teavel\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class ShopAuth
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $segments = $request->segments();
+
+        if (count($segments) === 3 && Auth::guest()) {
+            return redirect()->to(url($segments[0] . '/' . $segments[1]));
+        }
+
+        return $next($request);
+    }
+}
