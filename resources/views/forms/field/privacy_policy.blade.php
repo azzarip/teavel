cb313fd46a58c61d39be01501fc66ec1cd7603ff
Azzarip <paride21azzari@gmail.com>
2025-06-12 21:25:33 +0200

    fix: privacy policy can merge attributes

--- resources/views/forms/field/privacy_policy.blade.php
+++ resources/views/forms/field/privacy_policy.blade.php
@@ -2,7 +2,7 @@
     @error('privacy_policy')
         <p class="error-msg">{{ $message }}</p>
     @enderror
-    <div class="inline-flex items-center text-lg">
+    <div class="{{ $attributes->merge(['class' => 'inline-flex items-center']) }}">
         <input type="checkbox" name="privacy_policy" id="privacy_policy" @checked(old('privacy_policy')) class="w-4 h-4" required>
         <label for="privacy_policy" class="pl-2 ">{!! trans('teavel::forms.privacy_policy', ['link' => '<a href="/privacy-policy"
         class="inline-link">Privacy Policy</a>']) !!}</label>
