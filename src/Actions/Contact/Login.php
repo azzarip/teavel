8b5c6ca1eadc2b741d7ec7ec8c5bc371729cffb1
Azzarip <paride21azzari@gmail.com>
2025-08-05 01:00:22 +0200

    clearned login

--- src/Actions/Contact/Login.php
+++ src/Actions/Contact/Login.php
@@ -3,15 +3,13 @@
 namespace Azzarip\Teavel\Actions\Contact;
 
 use Azzarip\Teavel\Models\Contact;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Session;
 
 class Login
 {
     public static function login(Contact $contact)
     {
-        Auth::login($contact, true);
-        Session::remove('contact');
-        Session::regenerate();
+        auth()->login($contact, remember: true);
+        session()->remove('contact');
+        request()->session()->regenerate();
     }
 }
