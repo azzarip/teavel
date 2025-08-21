550414533e63fd61f8ac616e12e4b03e14e98ce1
Azzarip <paride21azzari@gmail.com>
2025-08-21 22:07:26 +0200

    new email details

--- resources/views/forms/field/email.blade.php
+++ resources/views/forms/field/email.blade.php
@@ -1,9 +1,14 @@
 @props(['email' => null])
 <div>
     <label for="email" class="block">Email:</label>
-    <input type="text" id="email" name="email"
+    <input id="email" name="email"
+        type="email"
+        inputmode="email"
+        autocomplete="email"
+        aria-describedby="email-help"
         class="input-text"
-        placeholder="@lang('teavel::forms.email.placeholder')" autocomplete="email" value="{{ $email ?? old('email') }}"
+        placeholder="@lang('teavel::forms.email.placeholder')" 
+        value="{{ $email ?? old('email') }}"
         required />
     @error('email')
         <p class="error-msg">{{ $message }}</p>
