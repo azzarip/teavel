54c907da1dccfa9582b607c474d94d5b8474e258
Azzarip <paride21azzari@gmail.com>
2025-08-21 22:37:09 +0200

    improved privacy policy

--- resources/views/forms/field/privacy_policy.blade.php
+++ resources/views/forms/field/privacy_policy.blade.php
@@ -2,9 +2,14 @@
     @error('privacy_policy')
         <p class="error-msg">{{ $message }}</p>
     @enderror
-    <div {{ $attributes->merge(['class' => 'inline-flex items-center']) }}>
-        <input type="checkbox" name="privacy_policy" id="privacy_policy" @checked(old('privacy_policy')) class="w-4 h-4" required>
-        <label for="privacy_policy" class="pl-2 ">{!! trans('teavel::forms.privacy_policy', ['link' => '<a href="/privacy-policy"
+    <div {{ $attributes->merge(['class' => 'inline-flex items-start']) }}>
+        <input 
+            type="checkbox" 
+            name="privacy_policy" id="privacy_policy" 
+            @checked(old('privacy_policy')) 
+            class="w-4 h-4 mt-1" 
+            required>
+        <label for="privacy_policy" class="ml-2">{!! trans('teavel::forms.privacy_policy', ['link' => '<a href="/privacy-policy"
         class="inline-link">Privacy Policy</a>']) !!}</label>
         </div>
 </div>
